17abcaf7be18e2da21bdceb89f0ca2f4f19f66e1
Tomi Akingba <akingba35@gmail.com>
2021-11-21 17:28:52 +0000

    Background Image implemented

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
+const img = require('./assets/clouds.jpg')
 export default function App() {
   return (
     <View style={styles.container}>
+      <ImageBackground source={img} style={styles.image}>
       <Text>Open up App.js to start! </Text>
       <StatusBar style="auto" />
+      </ImageBackground>
     </View>                                         
   );
 }
@@ -14,8 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  image:{
+    flex:1, 
+    resizeMode:"cover", 
+   // justifyContent:"Center"
+  }
 });
