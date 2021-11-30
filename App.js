8d4b5b65aa0c168c571f169d2d2cea735cdb6a74
Tomi Akingba <akingba35@gmail.com>
2021-11-30 14:53:13 +0000

    useEffect for location

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import DateTime from './components/DateTime'
 import WeatherScroll from './components/WeatherScroll'
@@ -7,6 +7,22 @@ import WeatherScroll from './components/WeatherScroll'
 
 const img = require('./assets/image.jpg')
 export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() =>{
+    navigator.geolocation.getCurrentPosition((sucess) => {
+      let {latitude, longitude} = success.coords;
+
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+        console.log(data)
+        showWeatherData(data);
+        })
+
+      })
+    }, [])
+
+
   return (
     <View style={styles.container}>
       <ImageBackground source={img} style={styles.image}>
--- components/DateTime.js
+++ components/DateTime.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {View, Text, StyleSheet} from 'react-native';
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 
 const WeatherItem = ({title, value, unit}) => {
@@ -13,25 +16,45 @@ const WeatherItem = ({title, value, unit}) => {
 }
 
 const DateTime = () => {
-    return(
-       
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
+
+    useEffect (() => {
+        setInterval(() => {
+            const time = new Date();
+            const month = time.getMonth();
+            const date = time.getDate();
+            const day = time.getDay();
+            const hour = time.getHours();
+            const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+            const minutes = time.getMinutes();
+            const ampm = hour >=12 ? 'pm' : 'am'
+        
+            setTime((hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes) +ampm) 
+            
+            setDate(days[day] + ', ' + date+ ' ' + months[month]) 
+        
+        }, 1000);
+    }, [])
+
+    return(       
        <View style={styles.container}>
             <View>
                  <View>
-                    <Text style={styles.heading}>11:39 AM</Text>
+                    <Text style={styles.heading}>{time}</Text>
                 </View>
                 <View>
-                    <Text style={styles.subheading}>Sunday, November 21</Text>
+                    <Text style={styles.subheading}>{date}</Text>
                 </View>
              <View style={styles.weatherItemContainer}>
-                <WeatherItem title ="Humidity" value="79" unit="%"/>
+                <WeatherItem title ="Humidity" value="78" unit="%"/>
                 <WeatherItem title ="Pressure" value="1000" unit="hpA"/>
                 <WeatherItem title ="Sunrise" value="09:00" unit="am"/>
                 <WeatherItem title ="Sunset" value="06:30" unit="pm"/>
             </View>
         </View>
         <View style={styles.rightAlign}>
-                 <Text style={styles.timezone}>Dublin,Ireland</Text>
+                 <Text style={styles.timezone}>Dublinnn,Ireland</Text>
                  <Text style={styles.latlong}>4.28N 50E</Text>
             </View>
         </View>    
