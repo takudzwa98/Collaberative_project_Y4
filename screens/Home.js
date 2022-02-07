443eb3b2641d801734ba3f894770fb3ece02a010
Amara <amara1092@gmail.com>
2022-02-07 17:45:49 +0000

    ProfileScreen updated

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { Animated, Dimensions, FlatList, Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Animated, Dimensions, FlatList, Image, Platform, StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
 import 'react-native-gesture-handler';
+import { DataTable } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // Plus...
@@ -286,7 +287,50 @@ function EmptyScreen() {
 function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+        <View style={styles.containerImage}>
+            <Image style={styles.bgImage} source={{
+                uri:"https://wallpapers.com/images/high/plain-color-wallpaper-6me8mv4y9zvjbdv2.jpg"
+                }} />
+                <View style={styles.bottomContainer}>
+                     <Image style={styles.profile} source={{
+                         uri:"https://scontent-dub4-1.xx.fbcdn.net/v/t1.6435-9/129762309_2248948631906378_3378543475299026941_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_ohc=XhbphQ6zEb8AX83lUpA&_nc_ht=scontent-dub4-1.xx&oh=bb8eddce558c37542b812c5088ad3ca2&oe=61D67DFA"
+                        }} />
+                    <Text>Oluwatomisin Akingba</Text>
+                    <Text style={{color:'gray',bottom:"7%"}}>Drogheda ,Ireland</Text> 
+                 
+                 <DataTable>
+                    <DataTable.Row>
+                        <DataTable.Cell>Matches played</DataTable.Cell>
+                        <DataTable.Cell numeric>5</DataTable.Cell>
+                    </DataTable.Row>
+
+                    <DataTable.Row>
+                        <DataTable.Cell>Goals</DataTable.Cell>
+                        <DataTable.Cell numeric>6</DataTable.Cell>
+                    </DataTable.Row>
+
+                    <DataTable.Row>
+                        <DataTable.Cell>Assists</DataTable.Cell>
+                        <DataTable.Cell numeric>3</DataTable.Cell>
+                    </DataTable.Row>
+
+                    <DataTable.Row>
+                        <DataTable.Cell>Yellow Cards</DataTable.Cell>
+                        <DataTable.Cell numeric>2</DataTable.Cell>
+                    </DataTable.Row>
+
+                    <DataTable.Row>
+                        <DataTable.Cell>Red Cards</DataTable.Cell>
+                        <DataTable.Cell numeric>0</DataTable.Cell>
+                    </DataTable.Row>
+
+                    <DataTable.Row>
+                        <DataTable.Cell>Trainings</DataTable.Cell>
+                        <DataTable.Cell numeric>10</DataTable.Cell>
+                    </DataTable.Row>
+                 </DataTable>
+                 </View>
+        </View>
     </View>
   );
 }
@@ -329,4 +373,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  bgImage:{
+    flex: 1,
+    position:'absolute',
+    width:'100%',
+    height:'100%'
+},
+bottomContainer:{marginTop:"50%", height:"90%", width:400, backgroundColor:'white', borderTopStartRadius: 50, borderTopEndRadius:50, alignItems:'center' },
+profile:{
+    height:120,
+    width:120, 
+    borderRadius: 25,
+    bottom:"10%"
+},
+head: {  height: 40,  backgroundColor: '#f1f8ff'  },
+wrapper: { flexDirection: 'row' },
+title: { flex: 1, backgroundColor: '#f6f8fa' },
+row: {  height: 28  },
+text: { textAlign: 'center' },
 });
